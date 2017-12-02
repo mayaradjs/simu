b9db81155fb64f9b1124218d08873b41435f3998
mayaradjs <maydkt@gmail.com>
2017-12-02 18:35:34 -0300

    lendo entradas 2

--- agencia.c
+++ agencia.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct dadosCliente{
 
@@ -9,21 +10,34 @@ typedef struct dadosCliente{
 
 
 
+int final=0, inicio=0;
+unsigned int tam;
+
+int insere (dadosCliente *F, dadosCliente cliente) {
+//  if ( (tam + inicio - final)%tam != 1 ) {
+     F[final] = cliente;
+     final = (final%tam)+1;
+     //return 0;
+//  }
+   //Fila cheia!!!);
+ // return 1;
+}
+
+
 int main(){
 
 	unsigned int n, m;
 
 	scanf ("%u %u", &m, &n);
+	tam = (int)pow(n, 0.5);	
 
-	dadosCliente filaDeAtendimento[n];	
+	dadosCliente filaAtendimento[tam], cliente;
 
 	for (int i=0; i<n; i++){
 		scanf("%u %u %u", &cliente.cod, &cliente.operacao, &cliente.valor);
-		
-		
-
+		insere(filaAtendimento, cliente);
 	}
-	
+
 
 
 return 0;
