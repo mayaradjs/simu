286cd41dc0cf46526ca126dcf482efec39858935
mayaradjs <maydkt@gmail.com>
2017-12-02 16:45:35 -0300

    lendo entradas

--- agencia.c
+++ agencia.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dadosCliente{
+
+	unsigned int cod, operacao, valor;
+	
+} dadosCliente;
+
+
 
 int main(){
 
-	long long int n, m;
-	scanf ("%lld %lld", &n, &m);
+	unsigned int n, m;
+
+	scanf ("%u %u", &m, &n);
+
+	dadosCliente filaDeAtendimento[n];	
+
+	for (int i=0; i<n; i++){
+		scanf("%u %u %u", &cliente.cod, &cliente.operacao, &cliente.valor);
+		
+		
+
+	}
 	
-	printf("teste\n");
 
 
 return 0;
